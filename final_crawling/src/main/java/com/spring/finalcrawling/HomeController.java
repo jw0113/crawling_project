0f88e41926a4e613cd5c8bcd2f78661bb1bb3ce0
jw0113 <jiwoo5430@gmail.com>
2021-12-22 13:09:13 +0900

    db 연동

--- final_crawling/src/main/java/com/spring/finalcrawling/HomeController.java
+++ final_crawling/src/main/java/com/spring/finalcrawling/HomeController.java
@@ -30,12 +30,15 @@ public class HomeController {
 	
 	// 사용자 정보를 받음
 	@RequestMapping(value = "/dataget", method = RequestMethod.POST)
-	public void dataget(DataVO vo) {
+	public String dataget(DataVO vo, Model model) {
 		
 		System.out.println("vo : " + vo.toString());
 		
 		// 사용자 정보를 보내기
 		String result = idataservice.dataSend(vo);
+		model.addAttribute("si", result);
+		
+		return "result";
 		
 	}
 	
